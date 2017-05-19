c5eef33abde9105651bae0335981668692e5286b
zluo16 <zluo@alumni.berklee.edu>
2017-05-19 16:42:56 -0400

    created anagrams class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    anas = []
+    array.each do |wrd|
+      if wrd.downcase.codepoints.sort == @word.downcase.codepoints.sort
+        anas << wrd
+      end
+    end
+    anas
+  end
+
+end
